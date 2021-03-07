bc9e1d97c9c63956153a01e5d2f24e0eb69dcc97
Samyak Sand <62803746+samyaksand@users.noreply.github.com>
2021-03-07 13:57:30 +0400

    Contribution for Day 6

---
+++ Day 6/CoreTeam/samyaksandday6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter Lower limit and Upper limit : ";
+    cin >> num1 >> num2;
+    int sum=0,even=0;
+
+    for(int i=num1+1; i<num2; i++)
+    {
+        sum=sum+i;
+        
+        if(i%2==0)
+            even=even+1;
+    }
+
+    cout<<"\nSum of all the numbers = "<<sum;
+    cout<<endl;
+    cout<<"Sum of all the even numbers = "<<even;
+    return 0;
+}
+
