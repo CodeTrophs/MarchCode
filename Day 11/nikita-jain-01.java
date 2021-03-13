9001a0934786c27b20aeb8348eb7c643dae20f1e
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-03-13 14:07:16 +0530

    Day 11 contribution

---
+++ Day 11/nikita-jain-01.java
@@ -0,0 +1,35 @@
+/*
+Input -> 15 35
+ Output -> 105
+*/
+
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int first=scan.nextInt();
+        int second=scan.nextInt();
+        int i,j,mul=1;
+        for(i=2;i<=first || i<=second;i++)
+        {
+            if(first%i==0 && second%i==0)
+            {
+                first=first/i;
+                second=second/i;
+                mul=mul*i;
+            }
+            else if(first%i==0 || second%i==0)
+            {
+                if(first%i==0)
+                    first=first/i;
+                else if(second%i==0)
+                    second=second/i;
+                mul=mul*i;
+            }
+        }
+        System.out.println(mul);
+    }
+}
