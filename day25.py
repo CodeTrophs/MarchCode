df63509ded0eb09b687fae6c25c2bb5954969684
ChetasShree <75165587+ChetasShree@users.noreply.github.com>
2021-03-27 07:43:49 +0530

    Updated Day 25

--- day25.py
+++ day25.py
@@ -1,30 +1,14 @@
 # To merge 2 array into a third array
 
-def merge_array(a,b):
-    sa = len(a)
-    sb = len(b)
+def merge_two_array(a,b):
     ans = []
-    i = 0
-    j = 0
-    while i<sa and j <sb:
-        if a[i] > b[j]:
-            ans.append(b[j])
-            j+=1
-        elif a[i] < b[j]:
-            ans.append(a[i])
-            i+=1
-    
-    while i<sa:
-        for m in range(sa-sb,sa):
-            ans.append(a[m])
-            i+=1
-    while j<sa:
-        for m in range(sb-sa,sb):
-            ans.append(b[m])
-            j+=1
-
+    for i in a:
+        ans.append(i)
+    for j in b:
+        ans.append(j)
     return ans
 
-a = [1,3,5,7,9,13]
-b = [2,4,6,8,10,24,34,67,69,74]
-print(merge_array(a,b))
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+print(merge_two_array(a,b))
